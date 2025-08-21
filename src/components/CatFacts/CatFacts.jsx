87929173c153e532a4c8a85e6ab53717e175b4f9
zabronax <zabronax@gmail.com>
2025-08-21 14:02:39 +0200

    fix cat fetcher

--- src/components/CatFacts/CatFacts.jsx
+++ src/components/CatFacts/CatFacts.jsx
@@ -1,28 +1,5 @@
 import { useEffect, useState } from "react"
 
-const facts = [
-  {
-    "fact": "Unlike dogs, cats do not have a sweet tooth. Scientists believe this is due to a mutation in a key taste receptor.",
-    "length": 114
-  },
-  {
-    "fact": "When a cat chases its prey, it keeps its head level. Dogs and humans bob their heads up and down.",
-    "length": 97
-  },
-  {
-    "fact": "The technical term for a cat’s hairball is a “bezoar.”",
-    "length": 54
-  },
-  {
-    "fact": "A group of cats is called a “clowder.”",
-    "length": 38
-  },
-  {
-    "fact": "A cat can’t climb head first down a tree because every claw on a cat’s paw points the same way. To get down from a tree, a cat must back down.",
-    "length": 142
-  }
-]
-
 export function CatFacts() {
   const [ data, setData ] = useState()
 
@@ -33,7 +10,7 @@ export function CatFacts() {
       const response = await fetch(url)
       const responseData = await response.json()
       console.log(responseData.data)
-      setData(responseData)
+      setData(responseData.data)
     }
     getFacts()
   }, [])
@@ -48,7 +25,7 @@ export function CatFacts() {
           !data ? <p>Loading</p>
           // If we have a response
           // show the list of facts
-          : facts.map((fact, index) => {
+          : data.map((fact, index) => {
               return (
                 <li key={index}>{fact.fact}</li>
               )
