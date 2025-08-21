ee471c210cb166bcf38b360ea163cacb234857ac
zabronax <zabronax@gmail.com>
2025-08-21 12:35:22 +0200

    setup tick function

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react"
+
 export function Timer() {
+  // Keeps track of seconds
+  const [time, setTime] = useState(0)
+
+  const hours = Math.floor(time / 3600)
+  const minutes = Math.floor((time % 3600) / 60)
+  const seconds = time % 60
+
+  function tick() {
+    setTime((oldTime) => oldTime + 1)
+  }
+
   return (
     <div>
       <p>
-        <span>00</span>
+        <span>{hours}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </p>
+
+      <button onClick={tick}>Debug</button>
     </div>
   )
 }
