09679886a8c3c62a9b62021805e50bea40397776
zabronax <zabronax@gmail.com>
2025-08-21 12:25:48 +0200

    setup timer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Timer } from './components/Timer/Timer'
 
 function App() {
   return (
@@ -7,6 +8,11 @@ function App() {
         <h1>React - useEffect</h1>
         <h2><a href="https://react.dev/reference/react/useEffect">useEffect Docs</a></h2>
       </header>
+
+      <section>
+        <h2>Timer Example</h2>
+        <Timer />
+      </section>
     </main>
   )
 }
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,13 @@
+export function Timer() {
+  return (
+    <div>
+      <p>
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </p>
+    </div>
+  )
+}
